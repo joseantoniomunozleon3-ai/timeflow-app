f4e0732b2c007b1adbf4c47950f58db05988a188
joseantoniomunozleon3-ai <joseantoniomunozleon3@gmail.com>
2026-05-03 02:16:58 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,30 +1,21 @@
-const CACHE_NAME = "timeflow-v1";
-const urlsToCache = [
-  "/",
-  "/index.html",
-  "/manifest.json"
+const CACHE_NAME = 'timeflow-v1';
+// Lista de archivos a guardar para usar sin internet
+const assets = [
+  './',
+  './index.html',
+  'https://cdnjs.cloudflare.com/ajax/libs/html2pdf.js/0.10.1/html2pdf.bundle.min.js'
 ];
 
-self.addEventListener("install", event => {
+self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME).then(cache => cache.addAll(assets))
   );
-  self.skipWaiting();
 });
 
-self.addEventListener("fetch", event => {
+self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
   );
-});
-
-self.addEventListener("activate", event => {
-  event.waitUntil(
-    caches.keys().then(keys => Promise.all(
-      keys.map(key => {
-        if (key !== CACHE_NAME) return caches.delete(key);
-      })
-    ))
-  );
-  self.clients.claim();
 });
